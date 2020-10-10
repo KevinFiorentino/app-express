22a76280fe67199ead6d44b48a8153e4c6ee1a60
kevin <kevinfiorentino.1294@gmail.com>
2020-10-10 14:03:47 -0300

    Ajustes URLs de los HTML

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* Login */
-router.get('/login', function (req, res, next) {
-    res.render('login', {});
+router.get('/login', function (req, res) {
+    //res.sendFile('/home/kevin/KevinWorkspace/app-express/public/login.html');
+    res.sendFile(process.cwd()+'/public/login.html');
 });
 
 /* Singup */
 router.get('/singup', function (req, res, next) {
-    res.render('singup', {});
+    res.sendFile(process.cwd()+'/public/singup.html');
 });
 
 /* Home page */
 router.get('/', function (req, res, next) {
-    res.render('index', {});
+    res.sendFile(process.cwd()+'/public/index.html');
 });
 
 module.exports = router;
