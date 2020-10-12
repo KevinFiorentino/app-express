c874449f731aace9c014e8ca2900ddb4d9730bb4
kevin <kevinfiorentino.1294@gmail.com>
2020-10-11 22:57:50 -0300

    Login con Passport

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const sassMiddleware = require('node-sass-middleware');
+const passport = require('./config/passport');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const routesPois = require('./routes/routesPois');
@@ -12,11 +14,24 @@ const routesToken = require('./routes/routesToken');
 
 const app = express();
 
+
+// Configuración Passport-Express
+const storeSession = new session.MemoryStore;
+app.use(session({
+    cookie: { maxAge: 86000 }, 
+    store: storeSession,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'kevin_secret'
+}));
+
 // Configuración de Express
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(cors());
 app.options('*', cors());
 app.use(sassMiddleware({
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const passport = require('../config/passport');
+
 
 /* Login */
 router.get('/login', function (req, res) {
@@ -8,14 +10,45 @@ router.get('/login', function (req, res) {
     res.sendFile(process.cwd()+'/public/login.html');
 });
 
+/* API Login */
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, usuario, info) {
+        if (err) return next(err)
+        if (!usuario) res.sendFile(process.cwd()+'/public/login.html');
+        req.logIn(usuario, function(err) {
+            if (err) return next(err);
+            res.sendFile(process.cwd()+'/public/index.html');
+        });
+    })(req, res, next);
+})
+
+/* API Logout */
+router.get('/logout', function(req, res) {
+    req.logOut();
+    res.sendFile(process.cwd()+'/public/login.html');
+})
+
 /* Singup */
 router.get('/singup', function (req, res, next) {
     res.sendFile(process.cwd()+'/public/singup.html');
 });
 
 /* Home page */
-router.get('/', function (req, res, next) {
+router.get('/', loggedIn, function (req, res, next) {
     res.sendFile(process.cwd()+'/public/index.html');
 });
+
+
+function loggedIn(req, res, next) {
+    console.log("LOGGEDIN")
+    if (req.user) {
+        next();
+    }
+    else {
+        console.log("Usuario no logeado")
+        res.redirect("/login")
+    }
+}
+
 
 module.exports = router;
