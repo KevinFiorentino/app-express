335bdf7d5ae7748d3e158f8e310cde000c2752cb
kevin <kevinfiorentino.1294@gmail.com>
2020-10-12 13:06:00 -0300

    Implementacion JWT

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const cors = require('cors');
 const sassMiddleware = require('node-sass-middleware');
 const passport = require('./config/passport');
 const session = require('express-session');
+const jwt = require('jsonwebtoken');
 
 const indexRouter = require('./routes/index');
 const routesPois = require('./routes/routesPois');
 const routesUsuarios = require('./routes/routesUsuarios');
 const routesToken = require('./routes/routesToken');
+const routesAuth = require('./routes/routesAuth');
 
 const app = express();
 
+app.set('secretKey', 'secret_kevin_jwt');
 
 // Configuración Passport-Express
 const storeSession = new session.MemoryStore;
@@ -45,9 +48,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas de la API
 app.use('/', indexRouter);
-app.use('/pois', routesPois);
+app.use('/pois', validarUsuarioJWT, routesPois);
 app.use('/users', routesUsuarios);
 app.use('/token', routesToken);
+app.use('/auth', routesAuth);
 
 
 const port = process.env.PORT || 8080;
@@ -55,6 +59,21 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`)
 })
+
+
+function validarUsuarioJWT(req, res, next) {
+    jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (err, decoded) {
+        if (err) {
+            res.json({ status: "error", message: err.message, data: null });
+        }
+        else {
+            req.body.userId = decoded.id;
+            console.log("JWT: ", decoded);
+            next();
+        }
+    })
+}
+
 
 
 module.exports = app;
