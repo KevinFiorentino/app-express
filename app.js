268f51091d8f0e466b60262c706662137bfda32c
kevin <kevinfiorentino.1294@gmail.com>
2020-10-13 18:09:19 -0300

    configurando Sengrid

--- app.js
+++ app.js
@@ -46,6 +46,15 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    // Dominio que tengan acceso (ej. 'http://example.com')
+       res.setHeader('Access-Control-Allow-Origin', '*');
+    // Metodos de solicitud que deseas permitir
+       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    // Encabecedados que permites (ej. 'X-Requested-With,content-type')
+       res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+})
 
 // Rutas de la API
 app.use('/', indexRouter);
