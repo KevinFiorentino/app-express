aa65527137d64e9daff45e864d28b1a08c0eb64b
kevin <kevinfiorentino.1294@gmail.com>
2020-10-12 22:25:35 -0300

    add environments var

--- config/configDataBase.js
+++ config/configDataBase.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/usuarios', {
+// mongodb+srv://kevin:kevinmongodb@curso-nodejs.qsx1k.mongodb.net/<dbname>?retryWrites=true&w=majority
+// mongodb://localhost/usuarios
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
